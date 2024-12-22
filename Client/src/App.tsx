1a4795dd818597fc244d256e9edf115c09e7f35c
Gracjan Grzech <g.grzech@icloud.com>
2024-12-22 23:18:13 +0100

    Add heroicons

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,14 @@
+import {CommandLineIcon} from "@heroicons/react/24/outline";
+
 export default function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-        <div className="card-bordered bg-base-100 p-1">
+        <div className="card-bordered rounded-lg bg-base-100 p-1">
             <div className="card-body">
-                <p className="text-3xl font-bold">Terminal</p>
+                <div className="flex flex-row justify-between items-center g-1">
+                    <p className="text-2xl">Terminal</p>
+                    <CommandLineIcon className="h-6 w-6 mt-0.5"/>
+                </div>
             </div>
         </div>
     </div>
