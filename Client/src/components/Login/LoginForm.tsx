2fe30b6aa33e52f1a184d36359c5c61503937b4e
Gracjan Grzech <g.grzech@icloud.com>
2025-02-02 14:38:44 +0100

    Use login mutation in component

--- Client/src/components/Login/LoginForm.tsx
+++ Client/src/components/Login/LoginForm.tsx
@@ -3,71 +3,81 @@ import TerminalBanner from "./TerminalBanner";
 import InputField from "./InputField";
 import SubmitButton from "./SubmitButton";
 import RememberMeButton from "./RememberMeButton";
+import { useLoginMutation } from "../../hooks/apiHooks";
+import { LoginRequest } from "../../api/terminalSchemas";
 
 const LoginForm = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [userData, setUserData] = useState({
-        email: "",
-        password: "",
-        rememberMe: false,
-    });
-    const [isEmailValid, setIsEmailValid] = useState(true);
+  const mutation = useLoginMutation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+    rememberMe: false,
+  });
+  const [isEmailValid, setIsEmailValid] = useState(true);
 
-    const validateEmail = (email: string) => {
-        const re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    };
+  const validateEmail = (email: string) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
 
-    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setUserData((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    }, []);
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }, []);
 
-    const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("form submitted");
-        const emailValid = validateEmail(userData.email);
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log("form submitted");
+      const emailValid = validateEmail(userData.email);
 
-        setIsEmailValid(emailValid);
+      setIsEmailValid(emailValid);
 
-        if (!emailValid) {
-            return;
-        }
+      if (!emailValid) {
+        return;
+      }
 
-        setIsLoading(true);
-        setTimeout(() => {
-            setIsLoading(false);
-            console.log(userData);
-        }, 2000);
-    }, [userData]);
+      setIsLoading(true);
 
-    return (
-        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-            <TerminalBanner />
-            <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4">
-                <InputField
-                    name="email"
-                    type="text"
-                    label="Email"
-                    value={userData.email}
-                    onChange={handleChange}
-                    isValid={isEmailValid}
-                />
-                <InputField
-                    name="password"
-                    type="password"
-                    label="Password"
-                    value={userData.password}
-                    onChange={handleChange}
-                />
-                <RememberMeButton />
-                <SubmitButton label="Sign in" isLoading={isLoading} />
-            </form>
-        </div>
-    );
+      const loginRequest: LoginRequest = {
+        email: userData.email,
+        password: userData.password,
+        twoFactorCode: "",
+        twoFactorRecoveryCode: "",
+      };
+      await mutation.mutateAsync(loginRequest);
+    },
+    [mutation, userData.email, userData.password],
+  );
+
+  return (
+    <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+      <TerminalBanner />
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4">
+        <InputField
+          name="email"
+          type="text"
+          label="Email"
+          value={userData.email}
+          onChange={handleChange}
+          isValid={isEmailValid}
+        />
+        <InputField
+          name="password"
+          type="password"
+          label="Password"
+          value={userData.password}
+          onChange={handleChange}
+        />
+        <RememberMeButton />
+        <SubmitButton label="Sign in" isLoading={isLoading} />
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,21 +1,27 @@
-import { StrictMode } from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import LoginPage from './pages/LoginPage.tsx'
+import { StrictMode } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 import Navbar from "./components/Navbar/Navbar.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-          <Navbar />
-          <Routes>
-              <Route path="/" element={ <App/> } />
-              <Route path="*" element={ <NotFoundPage/> } />
-              <Route path="/login" element={ <LoginPage/> } />
-          </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
