94c9ff6f8f18359e514df5785f2d6ba3e2e61a2e
Gracjan Grzech <g.grzech@icloud.com>
2025-02-02 15:33:25 +0100

    Work on auth

--- Client/src/hooks/apiHooks.ts
+++ Client/src/hooks/apiHooks.ts
@@ -2,9 +2,9 @@ import { useMutation } from "@tanstack/react-query";
 import { LoginRequest } from "../api/terminalSchemas";
 import axios from "axios";
 
-export function useLoginMutation(params: LoginRequest) {
+export function useLoginMutation() {
   const result = useMutation({
-    mutationFn: async () => {
+    mutationFn: async (params: LoginRequest) => {
       return await axios.post(
         "http://localhost:5006/api/v1/identity/login",
         params,
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,27 +1,10 @@
 import { StrictMode } from "react";
-import { BrowserRouter, Route, Routes } from "react-router";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import LoginPage from "./pages/LoginPage.tsx";
-import Navbar from "./components/Navbar/Navbar.tsx";
-import NotFoundPage from "./pages/NotFoundPage.tsx";
-import { QueryClient } from "@tanstack/query-core";
-import { QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
