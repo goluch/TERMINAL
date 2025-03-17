d5e2525da58a1f2d56ae84412ba77814241cf6ee
Gracjan Grzech <g.grzech@icloud.com>
2025-03-17 01:03:49 +0100

    Update styles for login component

--- Client/src/components/Login/LoginForm.tsx
+++ Client/src/components/Login/LoginForm.tsx
@@ -5,7 +5,7 @@ import SubmitButton from "../Shared/Forms/SubmitButton.tsx";
 import RememberMeButton from "./RememberMeButton";
 import { useLoginMutation } from "../../hooks/useLoginMutation.ts";
 import { LoginRequest } from "../../api/terminalSchemas";
-import { Navigate } from "react-router-dom";
+import { Navigate, redirect } from "react-router-dom";
 
 /**
  * LoginForm Component
@@ -14,7 +14,6 @@ import { Navigate } from "react-router-dom";
  *
  * @component
  * @param {LoginFormProps} props - The props for the LoginForm component
- * @returns {JSX.Element} - The rendered LoginForm component.
  */
 const LoginForm = () => {
     const mutation = useLoginMutation();
@@ -42,11 +41,13 @@ const LoginForm = () => {
             const loginRequest: LoginRequest = {
                 email: email,
                 password: password,
-                twoFactorCode: "",
-                twoFactorRecoveryCode: "",
             };
 
             await mutation.mutateAsync(loginRequest);
+
+            if (mutation.isSuccess) {
+                redirect("/");
+            }
         },
         [mutation, email, password],
     );
@@ -56,27 +57,43 @@ const LoginForm = () => {
     }
 
     return (
-        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-            <TerminalBanner />
-            <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4">
-                <InputField
-                    name="email"
-                    type="text"
-                    label="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    isValid={isEmailValid}
-                />
-                <InputField
-                    name="password"
-                    type="password"
-                    label="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <RememberMeButton />
-                <SubmitButton label="Sign in" isLoading={mutation.isPending} />
-            </form>
+        <div className="bg-white px-4 py-5 rounded-lg border-[1px] border-black/15 max-w-3xl w-full">
+            <div className="flex gap-5 h-full">
+                <div className="w-full flex-col gap-3 hidden sm:flex">
+                    <div className="border-[1px] [background-size:16px_16px] h-full flex justify-center items-center border-black/15 rounded-md bg-[radial-gradient(#e5e7eb_1px,transparent_1px)]">
+                        <TerminalBanner />
+                    </div>
+                </div>
+                <div className="w-full h-full">
+                    <div className="py-8 w-full">
+                        <p className="text-2xl font-normal text-center">Welcome back</p>
+                        <p className="text-sm font-normal text-center text-gray-600">Sign in to your account</p>
+                    </div>
+                    <form onSubmit={handleSubmit} className="w-full h-full flex flex-col gap-9">
+                        <div className="flex flex-col gap-3">
+                            <InputField
+                                name="email"
+                                type="text"
+                                label="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                isValid={isEmailValid}
+                            />
+                            <InputField
+                                name="password"
+                                type="password"
+                                label="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <SubmitButton label="Sign in" isLoading={mutation.isPending} />
+                            <p className="text-xs p-0 font-normal text-center text-gray-600">Don't have an account? Ask for an invitation</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- Client/src/components/Shared/Forms/SubmitButton.tsx
+++ Client/src/components/Shared/Forms/SubmitButton.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@headlessui/react';
+import { Button } from "@headlessui/react";
 import LoadingIndicator from "../../Login/LoadingIndicator.tsx";
 
 /**
  * Props interface for SubmitButton component
  */
 export interface SubmitButtonProps {
-  label: string;
-  isLoading?: boolean;
+    label: string;
+    isLoading?: boolean;
 }
 
 /**
@@ -16,20 +16,18 @@ export interface SubmitButtonProps {
  *
  * @component
  * @param {SubmitButtonProps} props - The props for the SubmitButton component
- * @returns {JSX.Element} - The rendered SubmitButton component.
  */
 const SubmitButton = ({ label, isLoading }: SubmitButtonProps) => (
-  <div className="w-full">
-    <Button
-      type="submit"
-      className="mt-8 w-full inline-flex items-center justify-center gap-2 rounded-xl bg-gray-600 py-2 px-4 text-base font-medium text-white shadow-inner shadow-white/10 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
-      disabled={isLoading}
-    >
-      {isLoading ? <LoadingIndicator /> : label}
-      {isLoading && 'Loading...'}
-    </Button>
-
-  </div>
+    <div className="w-full">
+        <Button
+            type="submit"
+            className="w-full inline-flex items-center justify-center gap-2 rounded-md bg-black py-2 px-4 text-base font-medium text-white shadow-inner shadow-white/10 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
+            disabled={isLoading}
+        >
+            {isLoading ? <LoadingIndicator /> : label}
+            {isLoading && "Loading..."}
+        </Button>
+    </div>
 );
 
 export default SubmitButton;
