a752355051647e3e5c7b7d28e0e64cb241ead13d
Gracjan <g.grzech@icloud.com>
2025-04-07 16:31:58 +0300

    Load user data into navbar

--- Client/src/components/Navbar/NavbarUserProfile.tsx
+++ Client/src/components/Navbar/NavbarUserProfile.tsx
@@ -1,13 +1,42 @@
+import useUserData from "@hooks/useUserData";
+
+function getUserRoleDisplayValue(role: string): string {
+  if (role === "Administrator") return "Administrator";
+  if (role === "Moderator") return "Moderator";
+  if (role === "Registered") return "Lab Worker";
+  if (role === "Guest") return "Guest";
+
+  throw new Error("Invalid role");
+}
+
+function getAvatarColor(email: string): string {
+  const hash = email
+    .split("")
+    .reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  const hue = hash % 360;
+  return `hsl(${hue}, 50%, 50%)`;
+}
+
 const NavbarUserProfile = () => {
+  const { data, status } = useUserData();
+
+  if (status === "pending") return <div>Loading...</div>;
+  if (status === "error") return <div>Error...</div>;
+
   return (
     <div className="p-2 bg-white rounded-b-md">
       <div className="flex gap-3 rounded-md p-2 hover:bg-gray-200 hover:cursor-pointer">
-        <div className="bg-neutral text-neutral-content w-12 h-10 rounded-lg flex justify-center items-center">
+        <div
+          style={{ backgroundColor: getAvatarColor(data?.email) }}
+          className="text-white w-12 h-10 rounded-lg flex justify-center items-center"
+        >
           <span>GG</span>
         </div>
         <div className="flex flex-col justify-start w-full">
-          <p className="text-sm w-full">Gracjan Grzech</p>
-          <p className="text-xs text-gray-500">Lab worker</p>
+          <p className="text-sm w-full">{data?.email}</p>
+          <p className="text-xs text-gray-500">
+            {getUserRoleDisplayValue(data?.role)}
+          </p>
         </div>
       </div>
     </div>
