b308f954b05e3fe24cd01f103490665a3e0f15c2
Gracjan <g.grzech@icloud.com>
2025-04-07 18:38:49 +0300

    Adjust background color and add shadow

--- Client/src/components/Navbar/Navbar.tsx
+++ Client/src/components/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ import TerminalNavbarContent from "./TerminalNavbarContent";
 const Navbar = () => {
   return (
     <nav className="h-screen min-h-screen max-h-screen min-w-72 p-2 pe-0">
-      <div className="h-full hidden sm:inline-flex w-full flex-col justify-between rounded-md border border-solid bg-white border-gray-200">
+      <div className="h-full hidden sm:inline-flex w-full flex-col justify-between rounded-md border border-solid bg-white border-gray-200 shadow-sm">
         <div className="navbar-start w-full flex flex-col rounded-md bg-white">
           <div className="flex y-4 items-center bg-white p-4 rounded-t-md">
             <p className="text-xl font-semibold">Terminal</p>
--- Client/src/pages/layouts/AuthorizedLayout.tsx
+++ Client/src/pages/layouts/AuthorizedLayout.tsx
@@ -18,10 +18,10 @@ const AuthorizedNavbarLayout = ({ pageName }: AuthorizedNavbarLayoutProps) => {
   }
 
   return (
-    <div className="min-h-screen w-screen flex flex-col gap-2 sm:flex-row bg-gray-200">
+    <div className="min-h-screen w-screen flex flex-col gap-2 sm:flex-row bg-gray-100">
       <Navbar />
       <div className="p-2 ps-0 h-screen min-h-screen max-h-screen w-full">
-        <div className="bg-white rounded-md inline-block border border-gray-200 w-full h-full overflow-hidden">
+        <div className="bg-white rounded-md inline-block border border-gray-200 shadow-sm w-full h-full overflow-hidden">
           <div className="h-[60px] text-xl flex font-medium items-center px-4 rounded-md">
             {pageName}
           </div>
