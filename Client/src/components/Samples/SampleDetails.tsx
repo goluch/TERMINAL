4fe0f58fca7c9971da8772ea0c6b401ded56a784
Gracjan <g.grzech@icloud.com>
2025-04-11 01:54:35 +0200

    Adjust table sort icons so that it doesn't affect layout

--- Client/src/components/Samples/SampleDetails.tsx
+++ Client/src/components/Samples/SampleDetails.tsx
@@ -1,38 +1,50 @@
-import {SampleDetailsDto} from "@api/terminalSchemas.ts";
-
+import { SampleDetailsDto } from "@api/terminalSchemas.ts";
 
 export interface SampleDetailsProps {
   dataQuery: SampleDetailsDto | undefined;
 }
 
 const SampleDetails = (props: SampleDetailsProps) => {
-
-    const date = new Date (props.dataQuery?.createdAtUtc ?? "");
+  const date = new Date(props.dataQuery?.createdAtUtc ?? "");
 
   return (
-    <div className="card-body">
-      <div className="card-title text-4xl">Details</div>
-      <div className="divider"></div>
-      <div className="text-3xl mb-3">Sample Code: {props.dataQuery?.code}</div>
-    <div className="grid grid-cols-[25%_75%] gap-y-3">
-      <div className="font-bold">Created:</div>
-      <div>
-        {`
-            ${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}
-            ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}
-        `}
+    <div className="border border-gray-200 rounded-lg bg-white">
+      <div className="text-lg font-medium border-b border-gray-200 h-[40.5px] p-2 flex">
+        Details
       </div>
-      <div className="font-bold flex items-center">Tags:</div>
-      <div>
-          {props.dataQuery?.tags?.map((tag) =>
-              (<div key={tag.id} className="badge badge-lg hover:cursor-default p-3 rounded-2xl mr-1 my-1">{tag.name}</div>)
-          )}
+      <div className="p-2 flex flex-col gap-3">
+        <div className="font-bold">Sample Code: {props.dataQuery?.code}</div>
+        <div className="flex">
+          <div className="font-bold">Created:</div>
+          <div>
+            {`
+                ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}
+                ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}
+            `}
+          </div>
+        </div>
+        <div className="flex">
+          <div className="font-bold flex items-center">Tags:</div>
+          <div className="flex gap-1">
+            {props.dataQuery?.tags?.map((tag) => (
+              <div
+                key={tag.id}
+                className="border border-gray-200 hover:cursor-default p-1 w-fit rounded-2xl text-xs"
+              >
+                {tag.name}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex">
+          <div className="font-bold">Comment:</div>
+          <div>{props.dataQuery?.comment}</div>
+        </div>
+        <div className="flex">
+          <div className="font-bold">Number of steps: </div>
+          <div>{props.dataQuery?.steps?.length}</div>
+        </div>
       </div>
-      <div className="font-bold">Comment:</div>
-      <div>{props.dataQuery?.comment}</div>
-      <div className="font-bold">Number of steps: </div>
-      <div>{props.dataQuery?.steps?.length}</div>
-    </div>
     </div>
   );
 };
--- Client/src/components/Shared/Table/TableView.tsx
+++ Client/src/components/Shared/Table/TableView.tsx
@@ -1,3 +1,5 @@
+import ChevronDownIcon from "@heroicons/react/24/outline/ChevronDownIcon";
+import ChevronUpIcon from "@heroicons/react/24/outline/ChevronUpIcon";
 import { flexRender, Table } from "@tanstack/react-table";
 import clsx from "clsx";
 
@@ -12,7 +14,7 @@ export interface TableViewProps<T extends TableElement> {
 
 const TableView = <T extends TableElement>(props: TableViewProps<T>) => {
   return (
-    <table className="table overflow-auto">
+    <table className="table">
       <thead>
         {props.table.getHeaderGroups().map((headerGroup) => (
           <tr key={headerGroup.id}>
@@ -22,6 +24,7 @@ const TableView = <T extends TableElement>(props: TableViewProps<T>) => {
                   onClick={header.column.getToggleSortingHandler()}
                   className={clsx(
                     header.column.getCanSort() && "cursor-pointer select-none",
+                    "flex gap-2 items-center",
                   )}
                 >
                   {flexRender(
@@ -29,9 +32,11 @@ const TableView = <T extends TableElement>(props: TableViewProps<T>) => {
                     header.getContext(),
                   )}
                   {{
-                    asc: " 🔼",
-                    desc: " 🔽",
-                  }[header.column.getIsSorted() as string] ?? null}
+                    asc: <ChevronUpIcon className="h-3" />,
+                    desc: <ChevronDownIcon className="h-3" />,
+                  }[header.column.getIsSorted() as string] ?? (
+                    <ChevronDownIcon className="h-3 invisible" />
+                  )}
                 </div>
               </th>
             ))}
