113f762ca1a281f5c329b165823aa38a2a308f24
Hubert Wajda <hubert.wajda03@gmail.com>
2025-04-11 21:01:19 +0200

    add mobile navbar using drawer

--- Client/src/components/Navbar/Navbar.tsx
+++ Client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { CommandLineIcon } from "@heroicons/react/24/outline";
+import {CommandLineIcon} from "@heroicons/react/24/outline";
 import NavbarUserProfilePopover from "./NavbarUserProfile";
 import TerminalNavbarContent from "./TerminalNavbarContent";
 
@@ -10,24 +10,25 @@ import TerminalNavbarContent from "./TerminalNavbarContent";
  * @component
  */
 const Navbar = () => {
-  return (
-    <nav className="h-screen min-h-screen max-h-screen min-w-72 p-2 pe-0">
-      <div className="h-full hidden sm:inline-flex w-full flex-col justify-between rounded-md border border-solid bg-white border-gray-200 shadow-sm">
-        <div className="navbar-start w-full flex flex-col rounded-md bg-white">
-          <div className="flex y-4 items-center bg-white p-4 rounded-t-md">
-            <p className="text-xl font-semibold">Terminal</p>
-            <CommandLineIcon className="h-5 w-5" />
-          </div>
-          <div className="h-px border-t border-solid border-gray-200 w-full"></div>
-          <TerminalNavbarContent />
-        </div>
-        <div className="navbar-end w-full">
-          <div className="h-px border-t border-solid border-gray-200 w-full"></div>
-          <NavbarUserProfilePopover />
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="h-screen min-h-screen max-h-screen min-w-72 p-2 pe-0">
+            <div
+                className="h-full sm:inline-flex w-full flex-col justify-between rounded-md border border-solid bg-white border-gray-200 shadow-sm">
+                <div className="navbar-start w-full flex flex-col rounded-md bg-white">
+                    <div className="flex y-4 items-center bg-white p-4 rounded-t-md">
+                        <p className="text-xl font-semibold">Terminal</p>
+                        <CommandLineIcon className="h-5 w-5"/>
+                    </div>
+                    <div className="h-px border-t border-solid border-gray-200 w-full"></div>
+                    <TerminalNavbarContent/>
+                </div>
+                <div className="navbar-end w-full">
+                    <div className="h-px border-t border-solid border-gray-200 w-full"></div>
+                    <NavbarUserProfilePopover/>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
