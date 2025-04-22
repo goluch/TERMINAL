c58e894d45fb9a367c6389ecd4f85853ee0d4e0f
Hubert Wajda <hubert.wajda03@gmail.com>
2025-04-22 15:13:06 +0200

    fix view of details - wrap on mobile devices

--- Client/src/pages/UsersPage.tsx
+++ Client/src/pages/UsersPage.tsx
@@ -6,11 +6,11 @@ import UserDetails from "@components/Users/UserDetails.tsx";
 import {useUserDetails} from "@hooks/useUserDetailsQuery.ts";
 
 const UsersPage = () => {
-    const [sorting, setSorting] = useState<SortingState> ([]);
+    const [sorting, setSorting] = useState<SortingState>([]);
     const [pagination, setPagination] = useState<PaginationState>(
         {
             pageIndex: 0,
-            pageSize:10
+            pageSize: 10
         }
     )
 
@@ -20,20 +20,20 @@ const UsersPage = () => {
         desc: sorting[0]?.desc ?? true
     })
 
-    const [userDetailsId, setUserDetailsId] = useState<string|null>(null);
+    const [userDetailsId, setUserDetailsId] = useState<string | null>(null);
 
     const dataQueryUserDetails = useUserDetails(userDetailsId);
 
     return (
         <div className="min-h-screen bg-gray-100">
-            <div className="flex justify-center p-5 flex-wrap">
+            <div className="flex flex-wrap justify-center p-5 flex-wrap">
                 <div className="flex-1 bg-white p-3 rounded-md m-1">
                     {dataQueryUsers.isLoading ?
                         (
                             <div className="flex justify-center">
                                 <span className="loading loading-spinner loading-md"></span>
                             </div>
-                        ):
+                        ) :
                         (
                             <Users
                                 dataQuery={dataQueryUsers.data}
@@ -52,8 +52,8 @@ const UsersPage = () => {
                             <div className="flex justify-center">
                                 <span className="loading loading-spinner loading-md"></span>
                             </div>
-                        ):
-                        (<UserDetails dataQuery={dataQueryUserDetails.data} />)
+                        ) :
+                        (<UserDetails dataQuery={dataQueryUserDetails.data}/>)
                     }
                 </div>
             </div>
