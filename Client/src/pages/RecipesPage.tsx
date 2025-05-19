8ef60141f5f3709991dbf461a9b977aef03d74f1
Hubert Wajda <hubert.wajda03@gmail.com>
2025-05-19 22:39:12 +0200

    add deleting recipes and fix scrollbar

--- Client/src/pages/RecipesPage.tsx
+++ Client/src/pages/RecipesPage.tsx
@@ -4,6 +4,7 @@ import {useRecipes} from "@hooks/recipes/useGetRecipes.ts";
 import Recipes from "@components/Recipes/Recipes.tsx";
 import {useRecipeDetails} from "@hooks/recipes/useGetRecipeDetails.ts";
 import RecipeDetails from "@components/Recipes/RecipeDetails.tsx";
+import {useDeleteRecipe} from "@hooks/recipes/useDeleteRecipe.ts";
 
 const RecipesPage = () => {
     const [sorting, setSorting] = useState<SortingState>([]);
@@ -22,39 +23,49 @@ const RecipesPage = () => {
 
     const dataQueryRecipeDetails = useRecipeDetails(recipeProjectId);
 
+    const mutation = useDeleteRecipe(
+        {
+            pageNumber: pagination.pageIndex,
+            pageSize: pagination.pageSize,
+            desc: sorting[0]?.desc ?? true
+        }
+    );
+
     return (
-        <div className="max-h-full flex bg-gray-100">
-            <div className="flex flex-wrap sm:flex-nowrap  max-h-full w-full justify-center p-1 gap-1">
-                <div className="flex-1 basis-2/5">
-                    {dataQueryRecipes.isLoading ? (
-                        <div className="flex justify-center">
-                            <span className="loading loading-spinner loading-md"></span>
-                        </div>
-                    ) : (
-                        <Recipes
-                            dataQuery={dataQueryRecipes.data}
-                            sorting={sorting}
-                            setSorting={setSorting}
-                            pagination={pagination}
-                            setPagination={setPagination}
-                            onChangeRecipeDetails={setRecipeDetailsId}
-                        />
-                    )}
-                </div>
-                <div className="flex-1 basis-3/5">
-                    {dataQueryRecipeDetails.isLoading ? (
-                        <div className="flex justify-center">
-                            <span className="loading loading-spinner loading-md"></span>
-                        </div>
-                    ) : recipeProjectId ? (
-                        <RecipeDetails dataQuery={dataQueryRecipeDetails.data}/>
-                    ) : (
-                        ""
-                    )}
+            <div className="max-h-full flex bg-gray-100">
+                <div className="flex flex-wrap sm:flex-nowrap  max-h-full w-full justify-center p-1 gap-1">
+                    <div className="flex-1 basis-2/5">
+                        {dataQueryRecipes.isLoading ? (
+                            <div className="flex justify-center">
+                                <span className="loading loading-spinner loading-md"></span>
+                            </div>
+                        ) : (
+                            <Recipes
+                                dataQuery={dataQueryRecipes.data}
+                                sorting={sorting}
+                                setSorting={setSorting}
+                                pagination={pagination}
+                                setPagination={setPagination}
+                                onChangeRecipeDetails={setRecipeDetailsId}
+                            />
+                        )}
+                    </div>
+                    <div className="flex-1 basis-3/5">
+                        {dataQueryRecipeDetails.isLoading ? (
+                            <div className="flex justify-center">
+                                <span className="loading loading-spinner loading-md"></span>
+                            </div>
+                        ) : recipeProjectId ? (
+                            <RecipeDetails dataQuery={dataQueryRecipeDetails.data}
+                                           mutateAsync={mutation.mutateAsync}
+                                           isPending={mutation.isPending}
+                            />
+                        ) : (
+                            ""
+                        )}
+                    </div>
                 </div>
             </div>
-
-        </div>
     );
 };
 
