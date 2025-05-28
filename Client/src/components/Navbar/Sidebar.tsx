aac3d182f600abddf32b4e86727e16b457f037e7
Gracjan <g.grzech@icloud.com>
2025-05-28 16:40:09 +0200

    Add logout functionality

--- Client/src/components/Navbar/Sidebar.tsx
+++ Client/src/components/Navbar/Sidebar.tsx
@@ -1,6 +1,6 @@
-import {CommandLineIcon} from "@heroicons/react/24/outline";
-import NavbarUserProfilePopover from "./SidebarUserProfile.tsx";
+import { CommandLineIcon } from "@heroicons/react/24/outline";
 import TerminalSidebarContent from "./TerminalSidebarContent.tsx";
+import SidebarUserProfile from "./SidebarUserProfile.tsx";
 
 /**
  * Sidebar Component
@@ -10,25 +10,24 @@ import TerminalSidebarContent from "./TerminalSidebarContent.tsx";
  * @component
  */
 const Sidebar = () => {
-    return (
-        <nav className="h-screen min-h-screen max-h-screen min-w-72 p-2 pe-0">
-            <div
-                className="h-full inline-flex w-full flex-col justify-between rounded-md border border-solid bg-white border-gray-200 shadow-sm">
-                <div className="navbar-start w-full flex flex-col rounded-md bg-white">
-                    <div className="flex y-4 items-center bg-white p-4 rounded-t-md">
-                        <p className="text-xl font-semibold">Terminal</p>
-                        <CommandLineIcon className="h-5 w-5"/>
-                    </div>
-                    <div className="h-px border-t border-solid border-gray-200 w-full"></div>
-                    <TerminalSidebarContent/>
-                </div>
-                <div className="navbar-end w-full">
-                    <div className="h-px border-t border-solid border-gray-200 w-full"></div>
-                    <NavbarUserProfilePopover/>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="h-screen min-h-screen max-h-screen min-w-72 p-2 pe-0">
+      <div className="h-full inline-flex w-full flex-col justify-between rounded-md border border-solid bg-white border-gray-200 shadow-sm">
+        <div className="navbar-start w-full flex flex-col rounded-md bg-white">
+          <div className="flex y-4 items-center bg-white p-4 rounded-t-md">
+            <p className="text-xl font-semibold">Terminal</p>
+            <CommandLineIcon className="h-5 w-5" />
+          </div>
+          <div className="h-px border-t border-solid border-gray-200 w-full"></div>
+          <TerminalSidebarContent />
+        </div>
+        <div className="navbar-end w-full">
+          <div className="h-px border-t border-solid border-gray-200 w-full"></div>
+          <SidebarUserProfile />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Sidebar;
