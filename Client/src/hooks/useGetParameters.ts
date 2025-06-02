d70db574093e46f9d2b9c622c1849bfecf190b10
Gracjan <g.grzech@icloud.com>
2025-06-02 23:29:15 +0200

    Fix linter errors

--- Client/src/hooks/useGetParameters.ts
+++ Client/src/hooks/useGetParameters.ts
@@ -1,5 +1,5 @@
 import apiClient from "@api/apiClient";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 type ParameterType = "decimal" | "text" | "integer";
 
