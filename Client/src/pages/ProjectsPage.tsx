5290ece055d6e274a9c70f4bff9b8f85fe813b79
Gracjan <g.grzech@icloud.com>
2025-06-05 21:54:37 +0200

    Fix lint errors

--- Client/src/pages/ProjectsPage.tsx
+++ Client/src/pages/ProjectsPage.tsx
@@ -2,9 +2,6 @@ import Projects from "@components/Projects/Projects.tsx";
 import { useState } from "react";
 import { PaginationState, SortingState } from "@tanstack/react-table";
 import { useProjects } from "@hooks/projects/useGetProjects.ts";
-import ProjectDetails from "@components/Projects/ProjectDetails.tsx";
-import { useProjectDetails } from "@hooks/projects/useGetProjectDetails.ts";
-import { useDeleteProject } from "@hooks/projects/useDeleteProject.ts";
 
 const ProjectsPage = () => {
   const [sorting, setSorting] = useState<SortingState>([]);
@@ -19,15 +16,9 @@ const ProjectsPage = () => {
     desc: sorting[0]?.desc ?? true,
   });
 
-  const mutation = useDeleteProject({
-    pageNumber: pagination.pageIndex,
-    pageSize: pagination.pageSize,
-    desc: sorting[0]?.desc ?? true,
-  });
-
+  // this is temporary
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [projectDetailsId, setProjectDetailsId] = useState<string | null>(null);
-
-  const dataQueryProjectDetails = useProjectDetails(projectDetailsId);
 
   return (
     <div className="h-full flex gap-3 flex-wrap sm:flex-nowrap justify-center p-3">
