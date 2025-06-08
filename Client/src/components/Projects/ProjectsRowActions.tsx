efda1e81d1b259eaa5c10e5edf16c73e00118e51
Hubert Wajda <hubert.wajda03@gmail.com>
2025-06-08 14:55:30 +0200

    set visibilty according to rules

--- Client/src/components/Projects/ProjectsRowActions.tsx
+++ Client/src/components/Projects/ProjectsRowActions.tsx
@@ -1,5 +1,6 @@
 import IconButton from "@components/Shared/IconButton";
 import { PencilIcon, XMarkIcon} from "@heroicons/react/24/outline";
+import VisibleForRoles from "@components/Shared/VisibleForRoles.tsx";
 
 type ProjectsRowActions = {
   onEdit: () => void;
@@ -8,18 +9,21 @@ type ProjectsRowActions = {
 const ProjectsRowActions = ({ onEdit, onDelete }: ProjectsRowActions) => {
   return (
     <div className="flex gap-1">
-     <IconButton
-         onClick={onEdit}
-         className="hover:bg-gray-100 hover:border-blue-200"
-     >
-         <PencilIcon className="h-4 rounded-md" />
-     </IconButton>
-      <IconButton
-        onClick={onDelete}
-        className="hover:bg-gray-100 hover:border-red-200"
-      >
-        <XMarkIcon className="h-4 rounded-md" />
-      </IconButton>
+        <VisibleForRoles roles={["Administrator", "Moderator"]}>
+         <IconButton
+             onClick={onEdit}
+             className="hover:bg-gray-100 hover:border-blue-200"
+         >
+             <PencilIcon className="h-4 rounded-md" />
+         </IconButton>
+          <IconButton
+            onClick={onDelete}
+            className="hover:bg-gray-100 hover:border-red-200"
+          >
+            <XMarkIcon className="h-4 rounded-md" />
+          </IconButton>
+        </VisibleForRoles>
+      <div className="m-4" />
     </div>
   );
 };
--- Client/src/components/Samples/SamplesRowActions.tsx
+++ Client/src/components/Samples/SamplesRowActions.tsx
@@ -1,5 +1,6 @@
 import IconButton from "@components/Shared/IconButton";
 import { EllipsisHorizontalIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import VisibleForRoles from "@components/Shared/VisibleForRoles.tsx";
 
 type SamplesRowActions = {
   onEdit: () => void;
@@ -14,12 +15,15 @@ const SamplesRowActions = ({ onEdit, onDelete }: SamplesRowActions) => {
       >
         <EllipsisHorizontalIcon className="h-4 rounded-md" />
       </IconButton>
-      <IconButton
-        onClick={onDelete}
-        className="hover:bg-gray-100 hover:border-red-200"
-      >
-        <XMarkIcon className="h-4 rounded-md" />
-      </IconButton>
+      <VisibleForRoles roles={["Administrator", "Moderator"]}>
+          <IconButton
+            onClick={onDelete}
+            className="hover:bg-gray-100 hover:border-red-200"
+          >
+            <XMarkIcon className="h-4 rounded-md" />
+          </IconButton>
+      </VisibleForRoles>
+      <div className="m-4" />
     </div>
   );
 };
