85bb4816481e5ef0a2c00d6af176db07c4a61908
OGHaruki <jajestemadam@gmail.com>
2025-06-09 20:45:23 +0200

    added row selection in projects page

--- Client/src/components/Projects/Projects.tsx
+++ Client/src/components/Projects/Projects.tsx
@@ -2,11 +2,11 @@ import {
   createColumnHelper,
   getCoreRowModel,
   OnChangeFn,
-  PaginationState,
+  PaginationState, Row,
   SortingState,
   useReactTable,
 } from "@tanstack/react-table";
-import { ProjectDto } from "@api/terminalSchemas.ts";
+import {ProjectDto} from "@api/terminalSchemas.ts";
 import { ProjectsResponse } from "@hooks/projects/useGetProjects.ts";
 import TableView from "@components/Shared/Table/TableView.tsx";
 import TableManagement from "@components/Shared/Table/TableManagment.tsx";
@@ -14,15 +14,21 @@ import TableCard from "@components/Shared/Table/TableCard";
 import ProjectsRowActions from "./ProjectsRowActions";
 import { Color } from "utils/colorUtils";
 import Chip from "@components/Shared/Chip";
-import {useMemo} from "react";
+import {useMemo, useState} from "react";
+import IndeterminateCheckbox from "@components/Shared/IndeterminateCheckbox.tsx";
+import InputField from "@components/Shared/InputField.tsx";
+import {MagnifyingGlassIcon, PlusIcon, XMarkIcon} from "@heroicons/react/24/outline";
+import VisibleForRoles from "@components/Shared/VisibleForRoles.tsx";
+import IconButton from "@components/Shared/IconButton.tsx";
+import {Link} from "react-router-dom";
 
 export interface ProjectsProps {
+  onChangeProjectDetails: (id: string) => void;
   projects: ProjectsResponse | undefined;
   sorting: SortingState;
-  setSorting: OnChangeFn<SortingState>;
   pagination: PaginationState;
+  setSorting: OnChangeFn<SortingState>;
   setPagination: OnChangeFn<PaginationState>;
-  onChangeProjectDetails: (id: string) => void;
   onEdit: (projectId: string) => void;
   onDelete: (projectId: string) => void;
 }
@@ -47,7 +53,28 @@ const columnHelper = createColumnHelper<ProjectDto>();
  * @component
  */
 const Projects = (props: ProjectsProps) => {
-  const columns = useMemo(()=>[
+
+  const [rowSelection, setRowSelection] = useState<Record<string, boolean>>({});
+
+  const columns = useMemo(() => [
+      {
+        id: "select-col",
+        size: 0,
+        header: ({ table }) => (
+          <IndeterminateCheckbox
+            checked={table.getIsAllRowsSelected()}
+            indeterminate={table.getIsSomeRowsSelected()}
+            onChange={table.getToggleAllRowsSelectedHandler()}
+          />
+        ),
+        cell: ({ row }: { row: Row<ProjectDto> }) => (
+            <IndeterminateCheckbox
+                checked={row.getIsSelected()}
+                disabled={!row.getCanSelect()}
+                onChange={row.getToggleSelectedHandler()}
+            />
+        ),
+      },
     columnHelper.accessor("name", {
       header: "Name",
       cell: (info) => info.getValue(),
@@ -84,7 +111,11 @@ const Projects = (props: ProjectsProps) => {
     state: {
       sorting: props.sorting,
       pagination: props.pagination,
+        rowSelection: rowSelection,
     },
+    getRowId: (row) => row.id,
+    onRowSelectionChange: setRowSelection,
+    enableMultiRowSelection: true,
     rowCount: props.projects?.rowsAmount ?? 0,
     onSortingChange: props.setSorting,
     onPaginationChange: props.setPagination,
@@ -96,11 +127,47 @@ const Projects = (props: ProjectsProps) => {
     props.onChangeProjectDetails?.(id?.toString() ?? "");
   };
 
+  const handleDeleteSelected = () => {
+    table.getSelectedRowModel().rows.forEach((row) => {
+        props.onDelete(row.original.id);
+    })
+  }
+
   return (
-    <TableCard className="!h-full">
-      <TableView<ProjectDto> table={table} handleClickRow={handleClick} />
-      <TableManagement<ProjectDto> table={table} />
-    </TableCard>
+      <>
+        <div className="flex justify-between gap-1 items-end pb-3 h-14">
+          <InputField
+              className="!text-sm !h-[40px]"
+              placeholder="Search"
+              icon={<MagnifyingGlassIcon className="h-4" />}
+          />
+          <VisibleForRoles roles={["Administrator", "Moderator"]}>
+            <div className="flex gap-1">
+              <IconButton
+                  onClick={handleDeleteSelected}
+                  disabled={
+                    !(table.getIsSomeRowsSelected() || table.getIsAllRowsSelected())
+                  }
+                  className="h-[40px] flex bg-white items-center gap-1 !hover:border-red-200"
+              >
+                <XMarkIcon className="h-4 " />
+                <p className="text-xs">Delete Selected</p>
+              </IconButton>
+              <Link to="/new-project">
+                <IconButton className="h-[40px] flex bg-white items-center gap-1">
+                  <PlusIcon className="h-4" />
+                  <p className="text-xs">Add new</p>
+                </IconButton>
+              </Link>
+            </div>
+          </VisibleForRoles>
+        </div>
+        <TableCard className="!h-full">
+          <TableView<ProjectDto> table={table} handleClickRow={handleClick} />
+          <TableManagement<ProjectDto> table={table} />
+        </TableCard>
+        );
+      </>
   );
 };
 
